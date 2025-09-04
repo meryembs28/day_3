1f2821fe0da103643f5a3a26e84f0d71404303f4
Meryem Bouras <bourasmeryem03@gmail.com>
2025-09-04 11:57:17 +0100

    c2

---
+++ chal2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ( ) {
+ int n ,l , c,i ;
+ printf("veuillez saisir un nbr de lignes svpp :");
+ scanf("%d", &n);
+ for (l=1 ; l<=n ; l++){
+    
+        for(i=1; i <= (n-l); i++)
+        {
+            printf("   ");
+        }
+        for(c=1; c<=(2*l - 1) ; c++){
+        printf ( " * " );
+        }
+        printf("\n");
+ }
+}
